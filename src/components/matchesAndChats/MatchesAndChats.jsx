172a9fb977a89499b247e58f055157e59cd1dc4f
alejandro-dominguez <alejandro.dominguez1@live.com>
2023-01-06 05:04:29 -0300

    save

--- src/components/matchesAndChats/MatchesAndChats.jsx
+++ src/components/matchesAndChats/MatchesAndChats.jsx
@@ -1,5 +1,9 @@
 import profile from '../../assets/horacio.png';
 import { FaHeart, FaCommentAlt } from "react-icons/fa";
+import { useEffect, useState } from 'react';
+import { onSnapshot, query, collection, where } from 'firebase/firestore';
+import { db } from '../../firebase/config';
+import useAuth from '../../hooks/useAuth';
 
 const MatchesAndChats = ({
     messagesState,
@@ -7,6 +11,20 @@ const MatchesAndChats = ({
     setMatchesState,
     setMessagesState
 }) => {
+    const [matches, setMatches] = useState([])
+    const { user } = useAuth()
+
+    useEffect(
+        () => 
+        onSnapshot(query(collection(db, "matches"), where("usersMatched", "array-contains", user.uid)),
+        (snapshot) => setMatches(
+            snapshot.docs.map((doc) => ({
+                id: doc.id,
+                ...doc.data()
+            }))
+        )),
+    [user, db])
+
     const handleMatches = () => {
         setMatchesState(true)
         setMessagesState(false)
@@ -42,16 +60,19 @@ const MatchesAndChats = ({
             </div>
             <div className="absolute -bottom-4 rounded-full h-[3px] w-[99.5%] bg-[#ed3434]/60" />
         </div>
-        {matchesState ?
+        {matchesState && !matches ?
         <div className="matchesAndChatsContainer">
             <div className="p-[0.225rem] rounded-lg gradientBgMatches matchesAndChatsBox cursor-pointer">
-                <div className="grid place-items-center p-[0.35rem] rounded-md bg-white transition-colors">
+                <div className=''>
+                    <img className='block w-full' src="" alt="Logo matcher corazón roto" />
+                </div>
+{/*                 <div className="grid place-items-center p-[0.35rem] rounded-md bg-white transition-colors">
                     <div style={{backgroundImage: `url(${profile})`}} className=" w-14 h-14 bg-cover bg-center
                     transition-colors bg-no-repeat rounded-full border-2 border-[#fb9bc3]" />
                     <p className="text-center text-sm font-bold mt-1 break-words textShadowSm transition-colors">
                         Horacio Perez
                     </p>
-                </div>
+                </div> */}
             </div>
         </div>
         : null}
@@ -60,13 +81,13 @@ const MatchesAndChats = ({
         border-[3px] border-[#ed3434]/60 mt-8 rounded-lg overflow-y-auto overflow-x-hidden scrollbar-thin
         scrollbar-track-[#ed3434]/30 scrollbar-thumb-[#ed3434]/90">
             <div className="w-full p-[0.225rem] rounded-lg gradientBgMatches matchesAndChatsBox cursor-pointer">
-                <div className="flex justify-start items-center p-2 gap-2 rounded-md bg-white transition-colors">
+{/*                 <div className="flex justify-start items-center p-2 gap-2 rounded-md bg-white transition-colors">
                     <div style={{backgroundImage: `url(${profile})`}} className=" w-12 h-12 bg-cover bg-center
                     bg-no-repeat rounded-full border-2 border-[#fb9bc3] transition-colors" />
                     <p className="text-center text-sm font-bold textShadowSm transition-colors">
                         Hola BB
                     </p>
-                </div>
+                </div> */}
             </div>
         </div>
         : null}
